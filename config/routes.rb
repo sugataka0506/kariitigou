24045793971c581501ac34b307c3b39fba6c81dd
“sugataka0506” <takapiro69@gmail.com>
2021-06-30 13:51:16 +0000

    add top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
+  get 'users/new'
+  get 'users/new2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
