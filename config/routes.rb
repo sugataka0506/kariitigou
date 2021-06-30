16cc063909ff148cc768fe54ed003a21ec31efc9
“sugataka0506” <takapiro69@gmail.com>
2021-06-30 13:58:36 +0000

    delete unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'users#index'
-
-  get 'users/new'
-  get 'users/new2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
