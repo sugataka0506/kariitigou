a324e4b955cd443e2e9a5bd504b7f1bd75ca45cc
“sugataka0506” <takapiro69@gmail.com>
2021-07-16 13:20:15 +0000

    tuika

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210630141019) do
+ActiveRecord::Schema.define(version: 20210716131154) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
