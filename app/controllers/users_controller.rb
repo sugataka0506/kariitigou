0b115fec0d13158f7169803e5cd0477df9143123
“sugataka0506” <takapiro69@gmail.com>
2021-08-11 13:57:46 +0000

    edit edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,15 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to edit_user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:image, :name, :description, :opinion, :email , :birthday, :region)
+  end
 end
