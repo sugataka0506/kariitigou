8970d2078fe8413632bd350601573544a85ed828
“sugataka0506” <takapiro69@gmail.com>
2021-09-03 13:57:01 +0000

    09031

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,9 @@ class UsersController < ApplicationController
     @users = User.where.not(id: current_user.id)
     @tags = Tag.all
   end
-  def show
-    @user = current_user
-  end
-  def edit
-    @user = current_user
-  end
-  def update
-    @user = current_user
-    @user.update(user_params)
-    redirect_to edit_user_path
+  def show 
+    @user = User.find_by(id: params[:id])
   end
   
   private
-  
-  def user_params
-    params.require(:user).permit(:image, :name, :description, :opinion, :email , :birthday, :region)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+  get "users/:id" => "users#show"
   
   resource :me, controller: :me
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
