a8a86250eb9242928d39261f76664168025092d5
“sugataka0506” <takapiro69@gmail.com>
2021-09-17 13:45:52 +0000

    aa

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,7 @@ class ChatsController < ApplicationController
   before_filter :authenticate_user!
      def index
          @receiver_id = params[:id]
-         @chats = Chat.all
+         @chats = Chat.where(sender_id: current_user.id, receiver_id: @receiver_id).or(Chat.where(sender_id: @receiver_id, receiver_id: current_user.id))
      end
      def create
         @chat =Chat.new(content: params[:content], sender_id: current_user.id,receiver_id: params[:id])
