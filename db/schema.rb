427ddd556d07ef88eedfd71da06e9e919c3bfac9
“sugataka0506” <takapiro69@gmail.com>
2021-09-17 14:11:31 +0000

    f

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210910130319) do
+ActiveRecord::Schema.define(version: 20210917140739) do
 
   create_table "chats", force: :cascade do |t|
     t.integer  "sender_id",   null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20210910130319) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_tags", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
