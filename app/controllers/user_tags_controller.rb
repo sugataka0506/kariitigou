32f0bb43d77626b8c1294a52fc21f18806becd8b
“sugataka0506” <takapiro69@gmail.com>
2021-09-23 14:25:03 +0000

    tag

---
+++ app/controllers/user_tags_controller.rb
@@ -0,0 +1,21 @@
+class UserTagsController < ApplicationController
+    def create
+      tag_name = params[:tag_name]
+      tag = Tag.find_by(name: tag_name)
+      if tag.nil?
+        tag = Tag.new(name: tag_name)
+        tag.save
+      end
+      user_tag = UserTag.find_by(user_id: current_user.id, tag_id: tag.id)
+      if user_tag.nil?
+        user_tag = UserTag.new(user_id: current_user.id, tag_id: tag.id)
+        user_tag.save
+      end
+      redirect_to("/me/edit")
+    end
+    def destroy
+        @user = current_user
+        @user.destroy
+        redirect_to("/me/edit")
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get "users/:id/chats" => "chats#index"
   post "users/:id/chats" => "chats#create"
   resource :me, controller: :me
+  post "user_tags" => "user_tags#update"
+  post "me/edit" => "me#edit"
+  post "me/destroy" => "me#destroy"
+  post "user_tags/create" => "user_tags#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
